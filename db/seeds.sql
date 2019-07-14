636bcbd8ba721bb877aeb0216761dc8bde0198cb
Sky Walker <Miles.Eddy1@Gmail.com>
2019-07-14 12:18:21 -0600

    add file structure and tables

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Alaskan Salmon', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Falafel', True);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Aged Angus Beef', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', True);
